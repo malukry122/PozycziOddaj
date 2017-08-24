335581a3961ff837e81f5fe33f27454a43e7a075
ADELL <malukry122@gmail.com>
2017-08-24 09:35:00 +0200

    ff

---
+++ src/main/java/com/example/adell/poyczioddaj/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.adell.poyczioddaj;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        //.setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, AddActivity.class));
+            }
+        });
+
+        TextView tv= (TextView) findViewById(R.id.textView);
+
+        BaseManager bm = new BaseManager(this);
+        //bm.addItem("plyta dvd", 1, "2017-08-30", "");
+        Cursor k = bm.getAllItems();
+        while(k.moveToNext()){
+            int nr=k.getInt(0);
+            String nazwa=k.getString(1);
+            int ilosc = k.getInt(2);
+            String data_do = k.getString(3);
+            String uwagi = k.getString(4);
+            tv.setText(tv.getText()+"\n"+nazwa+" "+ilosc+" "+data_do);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
